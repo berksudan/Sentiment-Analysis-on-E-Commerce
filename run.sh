06a706935f3dad5e36c1ffb01ac6b7889c0218b1
bs <23002942+berksudan@users.noreply.github.com>
2020-11-05 17:27:37 +0300

    run.sh added to run the program with ease

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-python3 main.py
+# Change current directory to the project directory.
+CURRENT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+# Paths of Python 3 and main module.
+PYTHON_BIN=python3
+PYTHON_MAIN=$CURRENT_PATH/main.py
+
+# Run the program.
+$PYTHON_BIN $PYTHON_MAIN
